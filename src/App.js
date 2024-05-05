6fae4b08aeded333b4371def82c2ed1a3bbe3c8e
kanishk-zoom <kanishk.sharma@zoomcar.com>
2024-05-06 02:50:01 +0530

    fix(some minor bug fixes for the weather api changes)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import WeatherForecast from './components/WeatherForecast';
 import ErrorModal from './components/ErrorModal';
 import './App.css'; // Main CSS for your app
